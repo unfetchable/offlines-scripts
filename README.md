bd3c98fba1817f126f23f6df89aecd5a9ddad731
unfetchable <mitu@vite.global>
2022-12-05 22:08:38 -0500

    yo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Credits
+   
+- Harmer#0123 - Owner and Programmer
+
+# Information
+Loadstring:
+```
+getgenv().key = "Vstra.V5_8e36047a9a0e05b8"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xandudiscord/vestra/main/loader.lua"))()
---
+++ loader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 292439477 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/unfetchable/phantom-forces/main/offlineEsp.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/unfetchable/phantom-forces/main/offlinesHitboxes.lua"))()
+end
+if game.PlaceId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Main", true))()
+end
