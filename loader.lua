1fa456073b379ad202cdcfe15b47736b8d125bb1
unfetchable <mitu@vite.global>
2022-12-05 22:19:08 -0500

    commit #3

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 292439477 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/unfetchable/phantom-forces/main/offlineEsp.lua", true))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/unfetchable/phantom-forces/main/offlinesHitboxes.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/unfetchable/offlines-scripts/main/Games/OfflineESP.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/unfetchable/offlines-scripts/main/Games/PhantomForcesHitboxes.lua"))()
 end
 if game.PlaceId == 286090429 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Main", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/unfetchable/offlines-scripts/main/Games/Arsenal.lua", true))()
 end
