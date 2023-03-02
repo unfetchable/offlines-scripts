f49b0a8bea11e5d5293dcf3ff764b33932aaef07
unfetchable <mitu@vite.global>
2023-03-02 15:51:36 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,3 +5,7 @@ end
 if game.PlaceId == 286090429 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/unfetchable/offlines-scripts/main/Games/Arsenal.lua", true))()
 end
+if game.PlaceId == 5938036553 then
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/unfetchable/offlines-scripts/main/Games/Frontlines/Hitboxes.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/unfetchable/offlines-scripts/main/Games/Frontlines/ESP.lua", true))()
+end
